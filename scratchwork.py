0f93239faae64c2a5bf6fc5dd89f6ecb5af1d19f
Alex Kolchinski <y.a.kolchinski@gmail.com>
2018-04-23 23:20:14 -0700

    A bunch of work in notebooks...

---
+++ scratchwork.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from mittens import GloVe
+import vsm
+import sst
+
+sst.build_dataset()
+
+vsm.ngram_vsm()
+
+pd.DataFrame
+
+def dice_distance(u,v):
+    np.min(u,v)
+    np.hstack(u,v)
